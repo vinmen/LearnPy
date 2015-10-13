0860d343fd40b60dc6654cf4d62984cce5c77ab0
Menon <moota123@gmail.com>
2015-10-13 18:36:34 -0400

    Added projectile.py

---
+++ projectile.py
@@ -0,0 +1,15 @@
+#Projectile Coordinates
+
+import math
+
+def plotXY():
+    for i in range(1, 100):
+        t = i * 0.1
+        x = 20.0 * t * math.cos(math.radians(70))
+        y = (20.0 * t * math.sin(math.radians(70))) - ((9.81 * (t**2))/2)
+        print(round(x), round(y), sep=',')
+        if y <= 0:
+            break
+
+if __name__ == '__main__':
+    plotXY()
