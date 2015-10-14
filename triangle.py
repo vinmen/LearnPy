2e0bddd275ab19137dbd2d7ee32b081fe36f4791
Vinod Menon <vinumenon@gmail.com>
2015-10-13 20:07:28 -0400

    Added triangle.py

---
+++ triangle.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# program to print * triangle for a give number.
+
+def triangle(number):    
+    output = ""    
+    for i in range(1, number + 1):           
+        for j in range(number - i):
+            output = output + " "            
+        for k in range(i):
+            output = output + "*" + " "                       
+        output = output + "\n"        
+    print(output)                                    
+    
+
+if __name__ == '__main__':
+    triangle(int(input('Enter Row Count : ')))
