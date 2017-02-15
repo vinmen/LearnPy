a2ef5bbc2d4a01a87641a682e891b4fa83d00e34
vinumen <vinumenon@gmail.com>
2017-02-15 16:00:25 -0500

    added alarm.py

---
+++ alarm.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+import sys
+
+def readFile(filepath):
+    with open(filepath) as f:
+        lines = f.read().splitlines()
+    for line in lines:
+        print(line)
+
+def getArgs(args):
+    for arg in args:
+        print(arg)
+
+if __name__ == "__main__":
+    getArgs(sys.argv)
+    readFile("/home/menon/py/learnPy/alarm.txt")
