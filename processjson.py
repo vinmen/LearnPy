aeed3b67fe6e4fb28ee7bee1ec0a749839f062c2
vinmen <vinumenon@gmail.com>
2018-05-10 23:03:29 -0400

    urllib2 update for python3

--- processjson.py
+++ processjson.py
@@ -1,20 +1,23 @@
-import urllib2
+#! /usr/bin/env python3
+
+import urllib.request
 import json
 
 def main():
 	dataurl = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"
-	webdata = urllib2.urlopen(dataurl)
+	response = urllib.request.urlopen(dataurl)
 
-	if webdata.getcode() == 200:
-		data = webdata.read()
-		jsondata = json.loads(data)
-
+	if response.getcode() == 200:
+		data = response.read()		
+		encoding = response.info().get_content_charset('utf-8')
+		jsondata = json.loads(data.decode(encoding))
+		f = open("/home/menon/Codes/py/learnPy/test.json", "w")
+		f.write(str(jsondata))
+		
 		if "title" in jsondata["metadata"]:
 			print(jsondata["metadata"]["title"])
-
-	else:
-		print("error")		
-
+		else:
+			print("error")
 
 if __name__ == '__main__':
 	main()
