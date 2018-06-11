b8703c8a83a4195e2c830096dcc5d13cc36d2d1f
Vinod Menon <vinumenon@gmail.com>
2018-06-11 17:54:17 -0400

    problem5 started

--- problem5.py
+++ problem5.py
@@ -1,8 +1,17 @@
-def find_large_number():
-    data = [50, 1, 2, 9]
-    temp = ''
+def problem5():
+    data = [1, 2, 3, 4, 5, 6, 7, 8, 9]   
+    temp = '' 
+    for x in range(1, 10):         
+        temp = temp + str(x)              
+        for y in range(3):
+            if x < 9:
+                if y == 0:
+                    temp = temp + '+'
+                elif y == 1:
+                    temp = temp + '-'
+                else:
+                    temp = temp + str(x+1)
+    print(temp)               
 
-    for i in range(len(data)):
-        temp_list = list(data).remove(data[i])
-        for j in temp_list:
-            temp = temp + data[j]
+ #1[+-2]2[+-3]3[+-4]4[+-5]5[+-6]6[+-7]7[+-8]8[+-9]
+problem5()
