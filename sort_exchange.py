8ef249f60969ca99203535eb7371f081e0fdf19b
vinmen <vinumenon@gmail.com>
2018-07-12 13:48:07 -0400

    Updated

--- sort_exchange.py
+++ sort_exchange.py
@@ -1,10 +1,12 @@
+
 def exchange_sort(data):    
     for i in range(len(data) - 1):
         for j in range(i + 1, len(data)):
             if data[j] < data[i]:
                 temp = data[i]
                 data[i] = data[j]
-                data[j] = temp   
+                data[j] = temp 
+                 
     print(data)
 
 if __name__ == "__main__":
