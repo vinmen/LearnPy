0d8375f267a772f59897f7dc7af50025f93fbbb1
vinmen <vinumenon@gmail.com>
2018-07-13 09:20:50 -0400

    Working on merge sort

--- sort_merge.py
+++ sort_merge.py
@@ -13,7 +13,21 @@ def merge_sort(data, i, j):
         output = merge(data1, data2)
     '''
 def merge(data1, data2):
-    pass
+    output = []
+    k = len(data1)
+    l = len(data2)
+    start = k
+
+    for i in range(k):
+        for j in range(l):
+            if i < k or j < l:
+                if data1[i] > data2[j]:
+                    output.append(data2[j])
+                    j = j + 1
+                else:
+                    output.append(data1[i])
+                    i = i + 1
+    
 
 if __name__ == "__main__":
     data = [10, 50, 60, 80, 20, 50, 100, 90, 30, 70]    
