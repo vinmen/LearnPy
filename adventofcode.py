0ee268d99131768141c30f0f8f1a8f39f1f08d66
vinmen <vinumenon@gmail.com>
2018-08-02 23:51:50 -0400

    Updated aoc

--- adventofcode.py
+++ adventofcode.py
@@ -1,59 +1,60 @@
 
-with open("adventofcode.txt") as f:
-    data = f.read().splitlines()
+def calculate_signal(circuits, item):
+    for data in circuits:
+        if data[0] != '' and data[0] == item[4]:
+            data[2] = item[5]
+        elif data[1] != '' and data[1] == item[4]:
+            data[3] = item[5]
 
-#data = ['x LSHIFT 2 -> y']
+def run():
+    with open("adventofcode.txt") as f:
+        data = f.read().splitlines()
+   
+    circuits = []
+    for line in data:
+        in_1 = ""
+        in_2 = ""
+        out = ""
+        gate = ""
+        signal = ""
 
-# in_1, in_2, out, gate, signal
-
-circuits = []
-for line in data:
-    in_1 = ""
-    in_2 = ""
-    out = ""
-    gate = ""
-    signal = ""
-
-    if line.find("AND") > -1:        
-        in_1 = line[0:line.find(" AND")]
-        in_2 = line[line.find(" AND") + 5:line.find(" ->")]
-        gate = "&"   
-    elif line.find("OR") > -1:
-        in_1 = line[0:line.find(" OR")]
-        in_2 = line[line.find(" OR") + 4:line.find(" ->")]
-        gate = "|"
-    elif line.find("LSHIFT") > -1:
-        in_1 = line[0:line.find("SHIFT") - 2]
-        in_2 = line[line.find("SHIFT") + 6:line.find(" ->")]
-        gate = "<<"
-    elif line.find("RSHIFT") > -1:
-        in_1 = line[0:line.find("SHIFT") - 2]
-        in_2 = line[line.find("SHIFT") + 6:line.find(" ->")]
-        gate = ">>"
-    elif line.find("NOT") > -1:        
-        in_1 = line[line.find("NOT") + 4:line.find(" ->")]
-        gate = "~"
-    else:
-        if line.find("a") > -1:
-            in_1 = line[0:line.find(" ->")]
+        if line.find("AND") > -1:        
+            in_1 = line[0:line.find(" AND")]
+            in_2 = line[line.find(" AND") + 5:line.find(" ->")]
+            gate = "&"   
+        elif line.find("OR") > -1:
+            in_1 = line[0:line.find(" OR")]
+            in_2 = line[line.find(" OR") + 4:line.find(" ->")]
+            gate = "|"
+        elif line.find("LSHIFT") > -1:
+            in_1 = line[0:line.find("SHIFT") - 2]
+            in_2 = line[line.find("SHIFT") + 6:line.find(" ->")]
+            gate = "<<"
+        elif line.find("RSHIFT") > -1:
+            in_1 = line[0:line.find("SHIFT") - 2]
+            in_2 = line[line.find("SHIFT") + 6:line.find(" ->")]
+            gate = ">>"
+        elif line.find("NOT") > -1:        
+            in_1 = line[line.find("NOT") + 4:line.find(" ->")]
+            gate = "~"
         else:
-            signal = line[0:line.find(" ->")]
+            gate = "-"
+            if line.find("a") > -1:
+                in_1 = line[0:line.find(" ->")]
+            else:
+                signal = line[0:line.find(" ->")]
 
-    out = line[line.find("->") + 3:]
-    circuits.append([in_1, in_2, out, gate, signal])
+        out = line[line.find("->") + 3:]
+        circuits.append([in_1, in_2, "", "", out, signal, gate])
 
-for item in circuits:
-    if item[4] != '':
+    for item in circuits:
+        if item[5] != '':
+            calculate_signal(circuits, item)
+
+    for item in circuits:
         print(item)
 
 
-def find_signal(data):
-    if data[4] != '':
-        for item in data:
-            if item[0] == item:
-                pass
-    else:
-        pass
-
-
-
+if __name__ == "__main__":
+    run()
+    
