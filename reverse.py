c031e6aa544234850604295f814fa2b6489664aa
Vinod Menon <vinumenon@gmail.com>
2023-07-06 13:56:52 -0400

    Added reverse.py

---
+++ reverse.py
@@ -0,0 +1,10 @@
+# Reverse string using recursion.
+
+def reverse(input):
+	if len(input) == 1:
+		return input
+	else:
+		return input[-1] + reverse(input[0:len(input) - 1])
+
+if __name__ == "__main__":
+  reverse("hello")
